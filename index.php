1ffe0d1920cf75926a9e6c29bda415ca4fdc7050
Osman Adak <osman.adak@gmail.com>
2017-09-13 09:39:38 +0300

    Remove

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<script type='text/javascript'> document.location = 'login.php'; </script>";
-?>
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'inc/core.php';
-if($us) {
-    $stmt = $dbh->prepare("DELETE FROM remember WHERE user_id='$us[id]'");
-    $stmt->execute();
-    session_destroy();
-}
-echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
-?>
